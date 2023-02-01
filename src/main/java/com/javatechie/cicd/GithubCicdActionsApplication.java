398c9179e4466a4ac2469c795482223273f6e637
pnnavadiya <pnavadiya@gmail.com>
2023-01-31 20:50:14 -0700

    code

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -14,12 +14,12 @@ public class GithubCicdActionsApplication {
 		return "Welcome to javatechie !";
 	}
 
-//	echo "# github-actions-example" >> README.md
+//	echo "# github-action-sample" >> README.md
 //	git init
 //	git add README.md
 //	git commit -m "first commit"
 //	git branch -M main
-//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
+//	git remote add origin https://github.com/payalsonani/github-action-sample.git
 //	git push -u origin main
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
